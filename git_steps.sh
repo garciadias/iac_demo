750d196c6a525880cc6e110c82f443b5abeeb80e
R. Garcia-Dias <rafaelgd@iac.es>
2017-11-24 11:58:47 +0000

    Add pair plot

---
+++ git_steps.sh
@@ -0,0 +1,7 @@
+git init
+git add *
+git commit -m 'Commit description'
+git status
+git log --graph --decorate --all --pretty=oneline
+git remote it remote add origin http://linkforyourserver.com
+git push -u origin master
