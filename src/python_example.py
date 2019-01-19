0c9c8cec8088a2e931d584556815f7ebbfc1763f
R. Garcia-Dias <rafaelagd@gmail.com>
2019-01-19 18:49:04 +0000

    implement 120 character limit.

--- src/python_example.py
+++ src/python_example.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 rep_path = os.getcwd()
-dset = pd.read_csv("%s/data/m67_photometry.csv" % rep_path,
-                   index_col=0)
+dset = pd.read_csv("%s/data/m67_photometry.csv" % rep_path, index_col=0)
 
 sns.pairplot(dset,
              x_vars=['JK'], y_vars=['J'],
